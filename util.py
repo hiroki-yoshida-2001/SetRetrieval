9cac6b5a6922b1a6a8141c7a049ee191df244339
s246312@wakayama-u.ac.jp <hiroki-yoshida-2001>
2024-10-10 23:31:24 +0900

    Editted for Conditional Set generation

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import matplotlib.pylab as plt
 import tensorflow as tf
 import numpy as np
 import os
+import math
 import pdb
 
 
@@ -26,18 +27,20 @@ def calc_set_sim_name(calc_set_sim):
 # parser for run.py
 def parser_run():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-mode', type=int, default=0, help='mode of computing set-matching score, maxPooling:0, poolingMA:1, CSS:2, setRepVec_biPMA:3, setRepVec_pivot:4, default:0')
+    parser.add_argument('-batch_size', type=int, default=50, help='The number of cordinate sets')
     parser.add_argument('-baseChn', type=int, default=32, help='number of base channel, default=32')
     parser.add_argument('-num_layers', type=int, default=3, help='number of layers (attentions) in encoder and decoder, default=3')
     parser.add_argument('-num_heads', type=int, default=5, help='number of heads in attention, default=5')
     parser.add_argument('-is_set_norm', type=int, default=1, help='switch of set-normalization (1:on, 0:off), default=1')
-    parser.add_argument('-is_cross_norm', type=int, default=1, help='switch of cross-normalization (1:on, 0:off), default=1')
     parser.add_argument('-trial', type=int, default=1, help='index of trial, default=1')
-    parser.add_argument('-calc_set_sim', type=int, default=0, help='how to evaluate set similarity, CS:0, BERTscore:1, default=0')
     parser.add_argument('-use_Cvec', type=int, default=1, help='Whether use Cvec')
-    parser.add_argument('-is_Cvec_linear', type=int, default=1, help='Whether divide FC_projection for Cluster seed vec and query')
+    parser.add_argument('-is_Cvec_linear', type=int, default=1, help='Whether learn FC_projection for Cluster seed vec')
     parser.add_argument('-pretrained_mlp', type=int, default=1, help='Whether pretrain MLP (not use FC_projection)')
-    parser.add_argument('-mlp_projection_dim', type=int, default=128, help='MLP will be learned to achieve designated dimention')
+    parser.add_argument('-mlp_projection_dim', type=int, default=128, help='MLP hidden last layer projects to the dimension')
+    parser.add_argument('-tf_data', type=int, default=0, help='0: numpy, 1: tf.data')
+    parser.add_argument('-category_emb', type=int, default=0, help='0: no category_emb, 1: implement category_emb')
+    parser.add_argument('-use_all_pred', type=int, default=0, help='0: set_to_item retrieval, 1: set_to_set retrieval')
+    parser.add_argument('-label_ver', type=int, default=1, help='0: use c2_label, 1: use c1_label')
 
     return parser
 #----------------------------
@@ -55,6 +58,7 @@ def parser_comp():
     parser.add_argument('-is_cross_norm', type=int, default=1, help='switch of cross-normalization (1:on, 0:off), default=1')
     parser.add_argument('-trials', default='1,2,3', help='list of indices of trials, default=1,2,3')
     parser.add_argument('-calc_set_sim', type=int, default=0, help='how to evaluate set similarity, CS:0, BERTscore:1, default=0')
+    
     return parser
 #----------------------------
 
@@ -183,7 +187,7 @@ def Set_hinge_loss(scores:tf.Tensor, y_true:tf.Tensor)->tf.Tensor:
     , getting positive score and the others by y_true (set labels, which identify positive position), and calculating hingeloss """
     
     Set_hinge_losssum = []
-    slack_variable = 0.2
+    slack_variable = 0.4
     # scores: (nSet_x, nSet_y), y_true: (nSet_x, nSet_y)
     for batch_ind in range(len(y_true)): 
         
@@ -196,7 +200,7 @@ def Set_hinge_loss(scores:tf.Tensor, y_true:tf.Tensor)->tf.Tensor:
 
         # hingeloss : (nSet_y - 1, )
         hingeloss = tf.maximum(negative_score - positive_score + slack_variable , 0.0)
-        Set_hinge_losssum.append(tf.reduce_mean(hingeloss))
+        Set_hinge_losssum.append(tf.reduce_sum(hingeloss))
         
 
     Loss = sum(Set_hinge_losssum)/len(Set_hinge_losssum)
@@ -204,12 +208,53 @@ def Set_hinge_loss(scores:tf.Tensor, y_true:tf.Tensor)->tf.Tensor:
     return Loss
 #----------------------------
 
-def Set_accuracy(score:tf.Tensor, y_true:tf.Tensor):
+def create_true_index(size):
+    # 初期化 - 単位行列を作成
+    matrix = tf.eye(size, dtype=tf.float32)
+
+    # 隣接するインデックスの入れ替え
+    for i in range(0, size, 2):
+        if i + 1 < size:
+            matrix = tf.tensor_scatter_nd_update(matrix, [[i, i+1], [i+1, i]], [1.0, 1.0])
+            matrix = tf.tensor_scatter_nd_update(matrix, [[i, i], [i+1, i+1]], [0.0, 0.0])
     
+    return matrix
+
+def Set_item_Cross_entropy(labels:tf.Tensor,  cos_sim: tf.Tensor)->tf.Tensor:
+
+    # 正解集合の特定に問題あり: y_true (set_label) を使って探すべき,..., 2変数しか入力できない
+    # 隣のインデックスがセットになっていると仮定
+    y_true = create_true_index(labels.shape[0])
+    set_size = tf.reduce_sum(tf.cast(labels != 41, tf.float32), axis=1)
+    pred_set_size = tf.gather(set_size, tf.where(tf.equal(y_true,1))[:,1])
+    # -------------------------------------
+    cce = tf.losses.CategoricalCrossentropy()
+    
+    batch_loss = []
+    for batch_ind in range(labels.shape[0]):
+        item_loss = []
+        for item_ind in range(labels.shape[1]):
+            target_cos_sim = cos_sim[batch_ind,:, item_ind, :]
+
+            target_label = labels[tf.where(tf.equal(y_true,1))[:,1][batch_ind]][item_ind]
+
+            if target_label == 41:
+                item_loss.append(0)
+            else:
+                binary_labels = tf.where(labels == target_label, 1.0, 0.0)
+
+                entropy_loss = cce(tf.reshape(binary_labels,-1), tf.reshape(target_cos_sim, -1))
+
+                item_loss.append(entropy_loss)
+        batch_loss.append(sum(item_loss) / pred_set_size[batch_ind])
+    
+    return sum(batch_loss)/len(batch_loss)
+
+def Set_accuracy(score, y_true):
     """Custom Metrics Function to evaluate set similarity between pred item set \hat y and gallery y"""
     """1 : positive_score is in top10 % of set similarity pairs , 0 : otherwise"""
     # threshold K 
-    threk = int(len(score)*0.1)
+    threk = int(len(score)*0.01)
     
     accuracy = np.zeros((len(score), 1))
 
@@ -222,58 +267,90 @@ def Set_accuracy(score:tf.Tensor, y_true:tf.Tensor):
     return accuracy
 
 #----------------------------
+# convert class labels to cross-set label（if the class-labels are same, 1, otherwise 0)
+def cross_set_label(y):
+    # rows of table
+    y_rows = tf.tile(tf.expand_dims(y,-1),[1,tf.shape(y)[0]])
+    # cols of table       
+    y_cols = tf.tile(tf.transpose(tf.expand_dims(y,-1)),[tf.shape(y)[0],1])
 
-# compute item wise similarity between pred Item and gallery and select item(in predict step)
-def item_selection(x, ID):
-    x, y = x # x, y : (nSet_x(y), nItemMax, dim)
-    category2, item_label, set_label = ID
-    nSet_x = tf.shape(x)[0]
-    nSet_y = tf.shape(y)[0]
-    nItemMax_y = tf.shape(y)[1]
+    # if the class-labels are same, 1, otherwise 0
+    labels = tf.cast(y_rows == y_cols, float)            
+    return labels
+
+def compute_test_rank(gallery, pred, set_label, category):
+
+    y_true = cross_set_label(set_label)
+    y_true = tf.linalg.set_diag(y_true, tf.zeros(y_true.shape[0], dtype=tf.float32))
+    # y_true = y_true[:100]
     
-    x_norm = x / np.linalg.norm(x, axis=-1, keepdims=True)
-    y_norm = y / np.linalg.norm(y, axis=-1, keepdims=True)
+    nItemMax_y = gallery.shape[1]
+    nItemPred = pred.shape[1]
+    x_norm = gallery / np.linalg.norm(gallery, axis=-1, keepdims=True)
+    y_norm = pred / np.linalg.norm(pred, axis=-1, keepdims=True)
+
+    # x_norm = x_norm[:100]
+    # y_norm = y_norm[:100]
     
+    nItem = np.count_nonzero(~np.all(gallery == 0, axis=-1), axis=1)
+
+    # nItem = nItem[:100]
+
     # コサイン類似度の計算
-    cos_sim = np.stack(
-        [
-            [
-                np.dot(y_norm[j], x_norm[i].T)
-                for i in range(len(x))
-            ]
-            for j in range(len(y))
-        ]
-    )
+    x_expand = np.expand_dims(x_norm, 0) # (nSet_x, 1, nItemMax, head_size)
+    y_expand = np.expand_dims(y_norm, 1) # (1, nSet_y, nItemMax, head_size)
+    cos_sim = np.einsum('aijk,ibmk->abjm', y_expand, x_expand, optimize='optimal') # (nSet_y, nSet_x, nItemMax_x, nItemMax_y)
+    pdb.set_trace()
     
-    # category2 label wise methods
-    expected_category2 = tf.constant([10001, 10002, 10003, 10004, 10005, 11001, 11002, 11003, 11004, 11005, 11006, 11007, 11008
-                        , 12001, 12002, 12003, 12004, 12005, 13001, 13002, 13003, 13004, 13005
-                        , 14001, 14002, 14003, 14004, 14005, 14006, 14007, 15001, 15002, 15003, 15004, 15005, 15006, 15007
-                        , 16001, 16002, 16003, 16004])
-    pred_id_data_batch = []
-    pred_id_data = []
+    def find_ranks(array, targets):
+        valid_indices = np.where(~np.isnan(array))[0]
+        valid_array = array.numpy()[valid_indices]
+        sorted_indices = np.argsort(valid_array)[::-1]
+        sorted_array = valid_array[sorted_indices]
+
+        if len(array.shape) ==1:
+            for target in targets:
+                if math.isnan(target):
+                    ranks = -1
+                else:
+                    rank = np.where(sorted_array == target)
+                    if len(rank) > 0:
+                        ranks = rank[0][0] + 1  # インデックスを1ベースに変換し、最も低いランクを追加
+                    else:
+                        ranks = -1  # 要素が見つからない場合は-1を追加
+        else:
+            ranks = []
+            for target in targets:
+                if math.isnan(target):
+                    ranks.append(-1)
+                else:
+                    rank = np.where(sorted_array == target)
+                    if len(rank) > 0:
+                        ranks.append(rank[0][0] + 1)  # インデックスを1ベースに変換し、最も低いランクを追加
+                    else:
+                        ranks.append(-1)  # 要素が見つからない場合は-1を追加
+
+        return ranks
+    ranks = []
     for batch_ind in range(len(cos_sim)):
-        for category2_ind in expected_category2:
-            indices = tf.where(category2 == category2_ind.numpy())
-            target_cos_sim = tf.stack([cos_sim[batch_ind][indices[i, 0], :, indices[i, 1]] for i in range(indices.shape[0])])
-            if len(target_cos_sim) == 0:
-                target_cos_sim = 0
-                pred_set_label = 0
-                pred_item_label = 0
-                pred_set_label = tf.cast(pred_set_label, tf.int64)
-                pred_item_label = tf.cast(pred_item_label, tf.int64)
+        rank = []
+        for item_ind in range(nItemPred):
+            if category[tf.where(tf.equal(y_true[batch_ind], 1))[0][0].numpy()][item_ind] != 0:
+                indices = tf.where(category == category[tf.where(tf.equal(y_true[batch_ind], 1))[0][0].numpy()][item_ind])
+                if len(indices[:,0]) == 1:
+                    rank.append(1)
+                else:  
+                    true_score = [cos_sim[batch_ind][:,item_ind,:][tf.where(tf.equal(y_true[batch_ind], 1))[0][0].numpy(), item_ind]]
+                    target_cos_sim = tf.stack([cos_sim[batch_ind][:,item_ind,:][indices.numpy()[i, 0], indices.numpy()[i, 1]] for i in range(indices.shape[0])])
+
+                    reshaped_array = target_cos_sim
+                    
+                    # target_score = tf.gather_nd(cos_sim[batch_ind][:,item_ind,:], tf.where(tf.equal(y_true[batch_ind], 1))).numpy()[0].tolist()
+                    rank.append(find_ranks(reshaped_array, true_score))
             else:
-                # 各ラベル毎の最近傍のIDさえ返せればいいです
-                flattened = tf.reshape(target_cos_sim, target_cos_sim.shape[0]*target_cos_sim.shape[1])
-                score, index = tf.math.top_k(flattened, k=1)
-                tiled_set_label = tf.tile(set_label, [1, item_label.shape[-1]])
-                pred_item_label = tf.gather_nd(item_label, indices[tf.unravel_index(index, target_cos_sim.shape)[0][0].numpy()])
-                pred_set_label = tf.gather_nd(tiled_set_label, indices[tf.unravel_index(index, target_cos_sim.shape)[0][0].numpy()])
-                pred_set_label = tf.cast(pred_set_label, tf.int64)
+                rank.append(-1)
 
-
-            pred_id_data.append(tf.stack([pred_set_label, pred_item_label],axis=0))
-        pred_id_data_batch.append(pred_id_data)
-        pred_id_data = []
-
-    return tf.stack(pred_id_data_batch)
+        ranks.append(rank)
+    
+    return ranks
+#----------------------------   
